139bb9f150181709cfb60d1ddb7ba6f8019dc713
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-08-02 09:28:03 +0100

    chore: report astrology 0.14.2

--- src/interface/runtime.ts
+++ src/interface/runtime.ts
@@ -6,7 +6,7 @@ import type { ApiRuntime } from "./api.js";
 
 export const loadApiRuntime = async (
   config: Config,
-  version = "0.14.1",
+  version = "0.14.2",
 ): Promise<ApiRuntime> => {
   const [{ service, options }, places] = await Promise.all([
     loadCalculationService(config, version),
