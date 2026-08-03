a912269597e5fb4e602eaa09437f43a19c3641fd
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-08-03 13:20:06 +0100

    Expose configured billing runtime type

--- src/interface/runtime.ts
+++ src/interface/runtime.ts
@@ -5,10 +5,15 @@ import { loadChartGenerationService } from "../generate/service.js";
 import { loadCscCatalogue } from "../place/csc.js";
 import type { ApiRuntime } from "./api.js";
 
+export interface LoadedApiRuntime extends ApiRuntime {
+  bills: BillStore;
+  openAiAdminKey: string | null;
+}
+
 export const loadApiRuntime = async (
   config: Config,
   version = "0.19.0",
-): Promise<ApiRuntime> => {
+): Promise<LoadedApiRuntime> => {
   const [{ service, options }, places] = await Promise.all([
     loadCalculationService(config, version),
     loadCscCatalogue(),
